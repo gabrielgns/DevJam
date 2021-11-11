edc8a4316396358b34868cdde88bdb7d1867a622
gabrielgns <gabriel.nascimento.santos@ccc.ufcg.edu.br>
2021-11-11 02:37:01 -0300

    last adjustment, did not finished in time

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
 	return (
 		<main>
 			<Routes>
-				<Route path='/login' element={<Login />} />
+				<Route path='/' element={<Login />} />
 				<Route path='/home' element={<Home />} />
 				<Route path='/:id' element={<Voting />} />
 			</Routes>
